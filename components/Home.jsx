1657a0efd093a311aa05da209581ba2d0253b485
Patrick Neschkudla <neschkudla@gmail.com>
2015-10-31 18:03:30 +0100

    initial commit

---
+++ components/Home.jsx
@@ -0,0 +1,7 @@
+HomeComp = React.createClass({
+  render() {
+    return (
+      <div>NO PLACE LIKE HOME!</div>
+    );
+  }
+});
---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+LayoutComp = React.createClass({
+  render() {
+    return (
+      <div>
+        (INSERT A HEADER COMPONENT HERE)
+        {this.props.content}
+        (INSERT SOME FOOTER HERE)
+      </div>
+    );
+  }
+});
